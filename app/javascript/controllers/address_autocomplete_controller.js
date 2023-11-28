78b2e8cbecaffad6b45449e51e7549f71949b3bf
Pontus Lofgren <pontus.loefgren@gmail.com>
2023-11-28 17:25:48 +0000

    Finished styling new form

--- app/javascript/controllers/address_autocomplete_controller.js
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -1,22 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+import mapboxgl from "mapbox-gl"
 
-// Connects to data-controller="address-autocomplete"
 export default class extends Controller {
   static values = { apiKey: String }
-  static targets = ["address"]
+  static targets = ["address", "map", "addressField"]
 
   connect() {
     console.log("Hello from geocoder")
+    console.log(this.addressTarget)
+    console.log(this.mapTarget)
 
+    this.marker = null; // used to reset marker
 
     this.geocoder = new MapboxGeocoder({
       accessToken: this.apiKeyValue,
       types: "country,region,place,postcode,locality,neighborhood,address",
     })
-    this.geocoder.addTo(this.element)
+
+    this.map = new mapboxgl.Map({
+      accessToken: this.apiKeyValue,
+      container: this.mapTarget,
+      style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb",
+      center: [18, 59],
+      zoom: 5
+    })
+
+    this.geocoder.addTo(this.addressFieldTarget)
     this.geocoder.on("result", event => this.#setInputValue(event))
-    this.geocoder.on("clear", () => this.#clearInputValue())
   }
 
   disconnect() {
@@ -25,9 +36,24 @@ export default class extends Controller {
 
   #setInputValue(event) {
     this.addressTarget.value = event.result["place_name"]
+    const longitude = event.result["center"][0]
+    const latitude = event.result["center"][1]
+    this.#addMarkersToMap(longitude, latitude);
+    this.#fitMapToMarkers(longitude, latitude)
   }
 
-  #clearInputValue() {
-    this.addressTarget.value = ""
-  }
+  #addMarkersToMap(long, lat) {
+    if (this.marker) {
+      this.marker.remove();
+    }
+    this.marker = new mapboxgl.Marker()
+      .setLngLat([ long, lat ])
+      .addTo(this.map)
+    }
+
+    #fitMapToMarkers(long, lat) {
+      const bounds = new mapboxgl.LngLatBounds()
+      bounds.extend([ long, lat ])
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+    }
 }
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+  LOCATIONS = [
+    'Outdoors',
+    'Centre',
+    'Virtual'
+  ]
 end
