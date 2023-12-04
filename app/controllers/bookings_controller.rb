6b1d07811f5097219551dae34fece7cdff2858b8
Pontus Lofgren <pontus.loefgren@gmail.com>
2023-12-04 22:14:19 +0000

    Stripe session on bookings create. Redirect to checkout page works

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,10 +7,32 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(bookings_params)
     @workout = Workout.find(params[:id])
+    @booking.amount = @workout.amount * @booking.attendees
+    @booking.state = 'pending'
     @booking.workout_id = @workout.id
     @booking.user_id = current_user.id
     if @booking.save
-      redirect_to profile_path(current_user)
+      session = Stripe::Checkout::Session.create({
+        line_items: [
+          {
+          price_data: {
+            currency: 'eur',
+            product_data: {
+              name: @workout.title,
+            },
+            unit_amount: @workout.amount,
+          },
+          quantity: @booking.attendees,
+        }
+      ],
+        mode: 'payment',
+        # These placeholder URLs will be replaced in a following step.
+        success_url: 'https://example.com/success',
+        cancel_url: 'https://example.com/cancel',
+      })
+
+      @booking.checkout_session_id = session.id
+      redirect_to session.url, allow_other_host: true
     else
       render :new, status: :unprocessable_entity
     end
