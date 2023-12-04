69910dda4e09c38380836d4a93140f9689d7484c
Pontus Lofgren <pontus.loefgren@gmail.com>
2023-12-04 11:23:59 +0000

    user must be booked to workout or workout owner to enter chat

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,11 @@
 class ChatroomsController < ApplicationController
 
   def show
-    @chatroom = Chatroom.find(params[:id])
-    @message = Message.new
+    chatroom = Chatroom.find(params[:id])
+    # user must be workout owner or have a booked spot to enter the chatroom
+    if chatroom.workout.user == current_user || chatroom.workout.bookings.users.include? == current_user
+      @chatroom = chatroom
+      @message = Message.new
+    end
   end
 end
